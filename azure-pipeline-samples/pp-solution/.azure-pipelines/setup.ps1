daf6cdebd9c32dc621bf4e4aee47729ca81f8afb
Pascal Soell <pascal.soell@digitall.com>
2024-11-27 11:12:19 +0100

    Fix name for solution repo #27

--- azure-pipeline-samples/pp-solution/.azure-pipelines/setup.ps1
+++ azure-pipeline-samples/pp-solution/.azure-pipelines/setup.ps1
@@ -112,7 +112,7 @@ $PipelineConfig = @{
 
     PipelineSource                  = @{
         RepositoryType   = "tfsgit";
-        RepositoryUrl    = "$ENV:DEVOPS_ORGANIZATION/$ENV:DEVOPS_PROJECT_NAME/_git/Solutions";
+        RepositoryUrl    = "$ENV:DEVOPS_ORGANIZATION/$ENV:DEVOPS_PROJECT_NAME/_git/$ENV:DEVOPS_PROJECT_NAME";
         RepositoryBranch = "main";
     };
 }
