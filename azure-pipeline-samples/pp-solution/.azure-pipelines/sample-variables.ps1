6aa0a56e1cd71b0615f034f575d47c80c5bedc11
Pascal Soell <pascal.soell@digitall.com>
2024-11-27 11:39:23 +0100

    Add sample environment variables

---
+++ azure-pipeline-samples/pp-solution/.azure-pipelines/sample-variables.ps1
@@ -0,0 +1,25 @@
+$ENV:AZURE_CLI_SUBSCRIPTTION_ID = "901d0ccc-2f11-46e6-9665-cafe033304fb"
+$ENV:AZURE_CLI_TENANT_ID = "901d0ccc-2f11-46e6-9665-cafe033304fb"
+
+$ENV:DEVOPS_ORGANIZATION = "https://dev.azure.com/digitallnature"
+$ENV:DEVOPS_PROJECT_NAME = "TEST"
+$ENV:DEVOPS_TENANT_ID = "901d0ccc-2f11-46e6-9665-cafe033304fb"
+
+# Default GitHub PAT you can use. If it is out-dated, create your own (with no additional permissions assigned).
+# See https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic
+$ENV:GITHUB_PAT = "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"
+
+$ENV:PP_CONN_DEV_APPID = "5b11b9fc-d02f-4643-8b70-5ebbb362b0cc"
+$ENV:PP_CONN_DEV_SECRET = "~4pP-s3Cr3t"
+$ENV:PP_CONN_DEV_TENANTID = "901d0ccc-2f11-46e6-9665-cafe033304fb"
+$ENV:PP_CONN_DEV_URL = "https://sample-dev.crm4.dynamics.com"
+
+$ENV:PP_CONN_PROD_APPID = "5b11b9fc-d02f-4643-8b70-5ebbb362b0cc"
+$ENV:PP_CONN_PROD_SECRET = "~4pP-s3Cr3t"
+$ENV:PP_CONN_PROD_TENANTID = "901d0ccc-2f11-46e6-9665-cafe033304fb"
+$ENV:PP_CONN_PROD_URL = "https://sample-test.crm4.dynamics.com/"
+
+$ENV:PP_CONN_TEST_APPID = "5b11b9fc-d02f-4643-8b70-5ebbb362b0cc"
+$ENV:PP_CONN_TEST_SECRET = "~4pP-s3Cr3t"
+$ENV:PP_CONN_TEST_TENANTID = "901d0ccc-2f11-46e6-9665-cafe033304fb"
+$ENV:PP_CONN_TEST_URL = "https://sample-prod.crm4.dynamics.com/"
