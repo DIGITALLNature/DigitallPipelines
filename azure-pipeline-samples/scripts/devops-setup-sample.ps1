6fc924782f68be023f23f4e56c6e3bb56f11e116
Pascal Soell <relexx@nanopix.de>
2025-01-14 17:31:01 +0100

    fix: typos and add some config samples (#30)
    * Fix name for solution repo globally #27
    * Fix template repo _git references
    * Add sample environment variables
    
    ---------
    
    Co-authored-by: Pascal Soell <pascal.soell@digitall.com>

--- azure-pipeline-samples/scripts/devops-setup-sample.ps1
+++ azure-pipeline-samples/scripts/devops-setup-sample.ps1
@@ -2,14 +2,15 @@ $DevOpsConfig = @{
     AzTenantId          = '901d0ccc-2f11-46e6-9665-cafe033304fb'
     DevOpsOrganization  = 'https://dev.azure.com/digitallnature'
     DevOpsProjectName   = 'TEST'
+    # Change the list of repositories to match your project setup, by default https://digitallnature.github.io/devops.html#repositories applies
     Repositories = @(
         @{
             Name = 'Serverside'
-            Template = 'https://digitallnature@dev.azure.com/digitallnature/Template%20Project/Serverside-Template'
+            Template = 'https://digitallnature@dev.azure.com/digitallnature/Template%20Project/_git/Serverside-Template'
         },
         @{
             Name = 'Clientside'
-            Template = 'https://digitallnature@dev.azure.com/digitallnature/Template%20Project/Clientside-Template'
+            Template = 'https://digitallnature@dev.azure.com/digitallnature/Template%20Project/_git/Clientside-Template'
         },
         @{
             Name = 'Documentation'
